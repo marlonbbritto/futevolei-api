552976522eff18615c9f00d4fd439cf839f9f797
marlonbbritto <marlonbbritto@gmail.com>
2025-05-19 00:46:50 -0300

    Doc(Championship): include swagger information

--- src/main/java/com/futevolei/championship/futevolei_api/controller/ChampionshipController.java
+++ src/main/java/com/futevolei/championship/futevolei_api/controller/ChampionshipController.java
@@ -3,6 +3,10 @@ package com.futevolei.championship.futevolei_api.controller;
 import com.futevolei.championship.futevolei_api.dto.championship.ChampionshipInsertDto;
 import com.futevolei.championship.futevolei_api.model.Championship;
 import com.futevolei.championship.futevolei_api.service.ChampionshipService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +22,12 @@ public class ChampionshipController {
     @Autowired
     private ChampionshipService championshipService;
 
+    @Operation(summary = "Create a new championship",
+            description = "Endpoint pto register a new championship in the system")
+    @ApiResponse(responseCode = "201", description = "Success to create a new championship",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Championship.class)))
+    @ApiResponse(responseCode = "400", description = "Invalid request data")
     @PostMapping
     public ResponseEntity<Championship> insert(@RequestBody ChampionshipInsertDto championship){
         Championship newChampionship = championshipService.insert(championship);
