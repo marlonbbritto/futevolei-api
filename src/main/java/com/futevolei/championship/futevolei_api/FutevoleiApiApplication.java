4dd822c14b5be7fcb01578564ac5b43bb5765968
marlonbbritto <marlonbbritto@gmail.com>
2025-05-18 21:31:06 -0300

    start the project

---
+++ src/main/java/com/futevolei/championship/futevolei_api/FutevoleiApiApplication.java
@@ -0,0 +1,13 @@
+package com.futevolei.championship.futevolei_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutevoleiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutevoleiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futevolei/championship/futevolei_api/FutevoleiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futevolei.championship.futevolei_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutevoleiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
