7059490cd101ea0c114f83864d214967bb6e1986
Marlon Barbosa Britto <marlon.britto@db1.com.br>
2025-05-20 14:22:17 -0300

    test(ChampionshipService): include test findById

--- src/test/java/com/futevolei/championship/futevolei_api/service/ChampionshipServiceTest.java
+++ src/test/java/com/futevolei/championship/futevolei_api/service/ChampionshipServiceTest.java
@@ -3,22 +3,24 @@ package com.futevolei.championship.futevolei_api.service;
 import com.futevolei.championship.futevolei_api.dto.championship.ChampionshipDto;
 import com.futevolei.championship.futevolei_api.model.Championship;
 import com.futevolei.championship.futevolei_api.repository.ChampionshipRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 
 @SpringBootTest
-public class ChampionShipServiceTest {
+public class ChampionshipServiceTest {
 
     @Autowired
     private ChampionshipService championshipService;
@@ -27,6 +29,7 @@ public class ChampionShipServiceTest {
     private ChampionshipRepository championshipRepository;
 
     @Test
+    @DisplayName("Should bring all registered championships when everything is correct.")
     void findAll_ReturnsListOfChampionshipDtos(){
         Championship championship1 = Championship.builder()
                 .id(1L)
@@ -51,5 +54,30 @@ public class ChampionShipServiceTest {
         assertEquals(2,dtoList.size());
         assertEquals("Teste 1",dtoList.get(0).name());
         assertEquals("Teste 2",dtoList.get(1).name());
+    }
+
+    @Test
+    @DisplayName("Should bring a specific championship when everything is right")
+    void findById_ReturnsSpecificChampionshipDtos(){
+
+        Championship championship1 = Championship.builder()
+                .id(1L)
+                .name("Teste 1")
+                .city("Maringá")
+                .startDate(LocalDate.now())
+                .numberOfTeams(3)
+                .build();
+        Long idToFind = championship1.getId();
+
+        when(championshipRepository.findById(idToFind)).thenReturn(Optional.of(championship1));
+
+        ChampionshipDto resultDto = championshipService.findById(idToFind);
+
+        assertNotNull(resultDto);
+
+        assertEquals(championship1.getName(),resultDto.name());
+        assertEquals(championship1.getCity(),resultDto.city());
+        assertEquals(championship1.getStartDate(),resultDto.startDate());
+        assertEquals(championship1.getNumberOfTeams(),resultDto.numberOfTeams());
     }
 }
