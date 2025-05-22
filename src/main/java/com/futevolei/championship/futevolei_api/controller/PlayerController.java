1cc2b18830ddf3feb148cdd0a6fd9faf24b621c5
Marlon Barbosa Britto <marlon.britto@db1.com.br>
2025-05-22 13:37:33 -0300

    Doc(Player): include informations for swagger

--- src/main/java/com/futevolei/championship/futevolei_api/controller/PlayerController.java
+++ src/main/java/com/futevolei/championship/futevolei_api/controller/PlayerController.java
@@ -1,8 +1,12 @@
 package com.futevolei.championship.futevolei_api.controller;
 
+import com.futevolei.championship.futevolei_api.dto.championship.ChampionshipDto;
 import com.futevolei.championship.futevolei_api.dto.player.PlayerDto;
 import com.futevolei.championship.futevolei_api.service.PlayerService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,11 +22,23 @@ public class PlayerController {
     @Autowired
     private PlayerService playerService;
 
+    @Operation(summary = "Find all players",
+    description = "Endpoint toshow all players registered")
+    @ApiResponse(responseCode = "200",
+    description = "Sucess to requisition")
     @GetMapping
     public ResponseEntity<List<PlayerDto>> findAll(){
         List<PlayerDto> playerDtosList = playerService.listAll();
         return ResponseEntity.ok().body(playerDtosList);
     }
+
+    @Operation(summary = "Find a specific player",
+    description = "Endpoint to show data of a specific player registered based on its ID")
+
+    @ApiResponse(responseCode = "200",
+            description = "Success: Player found",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = PlayerDto.class)))
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<PlayerDto> findById(@PathVariable Long id){
