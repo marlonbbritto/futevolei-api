dbbadc2cd682b93cf571936af46660e3cec3b2df
marlonbbritto <marlonbbritto@gmail.com>
2025-05-27 00:00:23 -0300

    feat(Exceptions): implement StandarError

---
+++ src/main/java/com/futevolei/championship/futevolei_api/exception/StandardError.java
@@ -0,0 +1,23 @@
+package com.futevolei.championship.futevolei_api.exception;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StandardError implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Instant timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+
+
+}
