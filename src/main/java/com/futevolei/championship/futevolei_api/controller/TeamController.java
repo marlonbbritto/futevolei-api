b84663667b930336938a6ee99b983864dc126e4e
Marlon Barbosa Britto <marlon.britto@db1.com.br>
2025-05-28 17:02:54 -0300

    feat(PlayerController): implement findById for PlayerController

--- src/main/java/com/futevolei/championship/futevolei_api/controller/TeamController.java
+++ src/main/java/com/futevolei/championship/futevolei_api/controller/TeamController.java
@@ -1,12 +1,16 @@
 package com.futevolei.championship.futevolei_api.controller;
 
+import com.futevolei.championship.futevolei_api.dto.player.PlayerDto;
 import com.futevolei.championship.futevolei_api.dto.team.TeamDto;
 import com.futevolei.championship.futevolei_api.service.TeamService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +30,19 @@ public class TeamController {
     public ResponseEntity<List<TeamDto>> findAll(){
         List<TeamDto> teamDtoList = teamService.findAll();
         return ResponseEntity.ok().body(teamDtoList);
+    }
+
+    @Operation(summary = "Find a specific team",
+            description = "Endpoint to show data of a specific team registered based on its ID")
+
+    @ApiResponse(responseCode = "200",
+            description = "Success: team found",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = TeamDto.class)))
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<TeamDto> findById(@PathVariable Long id){
+        TeamDto resultDto = teamService.findById(id);
+        return ResponseEntity.ok().body(resultDto);
     }
 }
